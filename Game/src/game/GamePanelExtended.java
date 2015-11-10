4383ac697a812e925d49361a3cee0c7982780c68
ArtixAllMighty <robbeenaxel@hotmail.com>
2015-11-10 22:53:23 +0100

    removed cursor from the game by implementing an invisible cursor

--- Game/src/game/GamePanelExtended.java
+++ Game/src/game/GamePanelExtended.java
@@ -1,5 +1,10 @@
 package game;
 
+import java.awt.Cursor;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+
 import engine.gamestate.GameStateManagerBase;
 import engine.window.GamePanel;
 
@@ -8,12 +13,15 @@ public class GamePanelExtended extends GamePanel{
 
 	public GamePanelExtended() {
 		super();
+		
+		BufferedImage cursorImg = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
+		// Create a new blank cursor.
+		Cursor blankCursor = Toolkit.getDefaultToolkit().createCustomCursor(cursorImg, new Point(0, 0), "blank cursor");
+		setCursor(blankCursor);
 	}
 	
 	@Override
 	protected GameStateManagerBase getGameStateManager() {
 		return new GameStateManager();
 	}
-	
-
 }
