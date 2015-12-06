cbfe0cc6c90d033aab77ad0effc40241528f7ca3
ArtixAllMighty <robbeenaxel@hotmail.com>
2015-12-06 23:42:52 +0100

    clean up

--- Game/src/engine/window/gameAid/Window.java
+++ Game/src/engine/window/gameAid/Window.java
@@ -10,14 +10,8 @@ public class Window {
 	private static int screenHeight;
 
 	public Window(){
-
-//		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		
-		screenWidth = 1366; //(int) screenSize.getWidth();//(int)w * (int)16;
-		
-		screenHeight = 768; //(int) screenSize.getHeight(); //(int)h * (int)9;
-		
-
+		screenWidth = 1366;
+		screenHeight = 768;
 	}
 	
 	public static int getWidth(){
